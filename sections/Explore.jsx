4a5933e38baf2bd89000e5564c37825ac883c679
Niket Rauniyar <vishal@gmail.com>
2025-02-10 15:53:10 +0530

    removing styles.js2

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -3,7 +3,6 @@
 import { useState } from "react"
 import { motion } from "framer-motion"
 
-import styles from "@/styles"
 import { staggerContainer } from "@/utils/motion"
 import { ExploreCard, TitleText, TypingText } from "@/components"
 import { exploreWorlds } from "@/constants"
